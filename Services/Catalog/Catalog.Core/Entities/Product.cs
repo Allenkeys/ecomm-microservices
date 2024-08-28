a825f95b4038c4d7b5bd9f3b14b32c847478bfb2
Joshua Eze <joshua.eze@cydene.com>
2024-08-28 18:33:16 +0100

    Add project files.

---
+++ Services/Catalog/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities;
+
+public class Product : BaseEntity
+{
+    [BsonElement("Name")]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    [BsonRepresentation(BsonType.Decimal128)]
+    public Decimal Price { get; set; }
+    public string PictureUrl { get; set; }
+    public ProductType ProductType { get; set; }
+    public Brand Brand { get; set; }
+    public int AvailableStock { get; set; }
+}
